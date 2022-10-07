5d67b22547e05ef4cc8e71798a64954e9881e641
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2022-10-07 17:58:26 +0200

    ajout reste javadoc

--- src/fr/istic/nplouzeau/cartaylor/api/Configurator.java
+++ src/fr/istic/nplouzeau/cartaylor/api/Configurator.java
@@ -3,13 +3,27 @@ package fr.istic.nplouzeau.cartaylor.api;
 import java.util.Set;
 
 public interface Configurator {
-
+/**
+ * 
+ * @return la liste des categories
+ */
     Set<Category> getCategories();
+    /**
+     * 
+     * @param category la categorie
+     * @return la liste des variants de cette catgories
+     */
 
     Set<PartType> getVariants(Category category);
-
+/**
+ * 
+ * @return la configuration 
+ */
     Configuration getConfiguration();
-
+/**
+ * 
+ * @return 
+ */
     CompatibilityChecker getCompatibilityChecker();
 
 }
