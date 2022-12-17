69a92d7178a7a46cca903676ed93aa9470b98830
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2022-12-17 22:45:05 +0100

    amélioration de la fonction init de configuratorImpl

--- src/fr/istic/nplouzeau/cartaylor/api/Configurator.java
+++ src/fr/istic/nplouzeau/cartaylor/api/Configurator.java
@@ -5,6 +5,12 @@ import java.util.Set;
 public interface Configurator {
 
     Set<Category> getCategories();
+
+    Set<PartType> getVariants(Category category);
+
+    Configuration getConfiguration();
+
     CompatibilityChecker getCompatibilityChecker();
+
 
 }
