a3a1d392e59129605ab72db097883f8d7b0e3c04
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2022-12-17 23:00:10 +0100

    ajout de getvariants dans ConfiguratorImpl

--- src/fr/istic/isaaaouad/impl/ConfiguratorImpl.java
+++ src/fr/istic/isaaaouad/impl/ConfiguratorImpl.java
@@ -1,2 +1,106 @@
-package fr.istic.isaaaouad.impl;public class ConfiguratorImpl {
+package fr.istic.isaaaouad.impl;
+
+import fr.istic.nplouzeau.cartaylor.api.*;
+
+import java.util.*;
+
+public class ConfiguratorImpl implements Configurator {
+
+    private ComptabilityManagerImpl cm;
+
+    private HashSet<PartType> allPart;
+    private Set<Category> categories;
+
+    private Configuration configuration;
+
+
+
+
+    CategoryImpl engine = new Engine();
+    CategoryImpl exterior = new Exterior();
+    CategoryImpl interior = new Interior();
+    CategoryImpl transmission = new Transmission();
+
+    public ConfiguratorImpl(){
+        this.cm = new ComptabilityManagerImpl();
+        this.categories  = new HashSet<>(Arrays.asList(engine,exterior,interior,transmission));
+
+        init();
+        this.configuration = new ConfigurationImpl(this.cm,new HashSet<>());
+
+    }
+
+    private void init(){
+        PartType eg100 = new PartTypeImpl("EG100",engine);
+        PartType eg133 = new PartTypeImpl("EG133",engine);
+        PartType eg210 = new PartTypeImpl("EG210",engine);
+        PartType ed110 = new PartTypeImpl("ED110",engine);
+        PartType ed180 = new PartTypeImpl("ED180",engine);
+        PartType eh120 = new PartTypeImpl("EH120",engine);
+
+        allPart.addAll(Arrays.asList(eg100,eg133,eg210,eh120,ed110,ed180,eh120));
+
+        PartType tm5 = new PartTypeImpl("TM5",transmission);
+        PartType tm6 = new PartTypeImpl("TM6",transmission);
+        PartType ta5 = new PartTypeImpl("TA5",transmission);
+        PartType ts6 = new PartTypeImpl("TS6",transmission);
+        PartType tsf7 = new PartTypeImpl("TSF7",transmission);
+        PartType tc120 = new PartTypeImpl("TC120",transmission);
+
+        allPart.addAll(Arrays.asList(tm5,tm6,ta5,ts6,tsf7,tc120));
+
+        cm.addIncompatibilities(ta5, new HashSet<>(Arrays.asList(eg100)));
+        cm.addIncompatibilities(tsf7, new HashSet<>(Arrays.asList(eg100,eg133,ed110)));
+        cm.addRequirements(tc120,new HashSet<>(Arrays.asList(eh120)));
+
+        PartType xc = new PartTypeImpl("XC",exterior);
+        PartType xm = new PartTypeImpl("XM",exterior);
+        PartType xs = new PartTypeImpl("XS",exterior);
+
+        cm.addIncompatibilities(xc, new HashSet<>(Arrays.asList(eg210)));
+        cm.addIncompatibilities(xm, new HashSet<>(Arrays.asList(eg100)));
+        cm.addIncompatibilities(xs, new HashSet<>(Arrays.asList(eg100)));
+
+
+        PartType in = new PartTypeImpl("IN",interior);
+        PartType ih = new PartTypeImpl("IH",interior);
+        PartType is = new PartTypeImpl("IS",interior);
+
+        allPart.addAll(Arrays.asList(in,ih,is));
+
+        cm.addIncompatibilities(is, new HashSet<>(Arrays.asList(eg100,tm5)));
+        cm.addRequirements(is,new HashSet<>(Arrays.asList(xs)));
+
+        cm.addRequirements(xs,new HashSet<>(Arrays.asList(is)));
+    }
+
+
+
+
+    @Override
+    public Set<Category> getCategories() {
+        return Collections.unmodifiableSet(this.categories);
+    }
+
+    @Override
+    public Set<PartType> getVariants(Category category) {
+        Objects.requireNonNull(category, "The category can't be null for getVariants");
+        Set<PartType> variants = new HashSet<>();
+        for(PartType p : allPart){
+            if(p.getCategory() instanceof Category){
+                variants.add(p);
+            }
+        }
+        return variants;
+    }
+
+    @Override
+    public Configuration getConfiguration() {
+        return this.configuration;
+    }
+
+    @Override
+    public CompatibilityChecker getCompatibilityChecker() {
+        return this.cm;
+    }
 }
