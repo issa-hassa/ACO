36d1661421a9f7cc322088abbcdf6e9c95f514cb
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2022-12-18 21:38:43 +0100

    Implementation de engine : commencement

--- src/fr/istic/Utils.java
+++ src/fr/istic/Utils.java
@@ -1,5 +1,17 @@
 package fr.istic;
 
 public class Utils {
+    public static final String POWER = "power";
+    public static final String TYPE = "type";
+
+    public static final String KW_100 = "100kw";
+
+
+    public  enum EngineType{
+        GASOLINE,
+        DIESEL,
+        HYBRID
+    }
+
 
 }
--- src/fr/istic/issaouad/impl/Engine.java
+++ src/fr/istic/issaouad/impl/Engine.java
@@ -1,11 +1,88 @@
 package fr.istic.issaouad.impl;
 
-public class Engine extends CategoryImpl {
+import fr.istic.Utils;
+import fr.istic.nplouzeau.cartaylor.api.PartType;
 
-	@Override
-	public boolean equals(Object o){
-		return( o instanceof Engine);
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Engine extends PartImpl {
+
+	protected Utils.EngineType engineType;
+
+	protected String power;
+
+	protected Set<String> typePossibilities;
+	protected Set<String> powerPossibilities;
+
+
+	public Engine(PartType type) {
+		super(type);
 	}
-	
+
+	protected String getengineType(){
+		return engineType.name();
+	}
+	protected void  setEngineType(String type) throws IllegalArgumentException {
+		if(!getAvailablePropertyValues(Utils.TYPE).contains(type)){
+			throw new IllegalArgumentException(type + " is not a type property");
+		}
+		this.engineType.valueOf(type);
+
+	}
+	protected String getPower(){
+		return this.power;
+	}
+
+	protected void  setEnginePower(String type) throws IllegalArgumentException {
+		if(!getAvailablePropertyValues(Utils.POWER).contains(type)){
+			throw new IllegalArgumentException(type + " is not a power property");
+		}
+	}
+
+	public static class EG100 extends Engine {
+
+		public EG100(PartType type) {
+			super(type);
+		}
+	}
+	public static class EG133 extends Engine {
+
+		public EG133(PartType type) {
+			super(type);
+		}
+	}
+	public static class EG210 extends Engine {
+
+		public EG210(PartType type) {
+			super(type);
+			this.engineType = Utils.EngineType.DIESEL;
+			this.typePossibilities = new HashSet<>(Arrays.asList(Utils.EngineType.GASOLINE.name()));
+			this.powerPossibilities = new HashSet<>(Arrays.asList(Utils.EngineType.GASOLINE.name()));
+			this.power = "100KW";
+
+		}
+	}
+	public static class ED110 extends Engine {
+
+		public ED110(PartType type) {
+			super(type);
+		}
+	}
+	public static class EG180 extends Engine {
+
+		public EG180(PartType type) {
+			super(type);
+		}
+	}
+	public static class EH120 extends Engine {
+
+		public EH120(PartType type) {
+			super(type);
+		}
+	}
+
+
 
 }
