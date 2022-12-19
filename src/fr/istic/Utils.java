32615c60845663f1fff81c8d04043d1136f6f5ff
issa-hassa <75831785+issa-hassa@users.noreply.github.com>
2022-12-19 12:34:13 +0100

    Implementation de Transmission

--- src/fr/istic/Utils.java
+++ src/fr/istic/Utils.java
@@ -6,14 +6,34 @@ public class Utils {
 
     public static final String PAINT = "paint";
 
+    public static final String TRANSMISSION_TYPE = "transmission_type";
+
+    public static final String TRANSMISSION_SPEED = "speed";
+
     public static final String QUALITY = "quality";
 
+    public static final String MAX_POWER  = "max_power";
+
+    public static final String WHEEL = "wheel";
+
     public static final String KW_100 = "100kw";
-    public static final String KW_133 = "100kw";
-    public static final String KW_210 = "100kw";
-    public static final String KW_110 = "100kw";
-    public static final String KW_180 = "100kw";
-    public static final String KW_120 = "100kw";
+    public static final String KW_133 = "133kw";
+    public static final String KW_210 = "210kw";
+    public static final String KW_110 = "110kw";
+    public static final String KW_180 = "180kw";
+    public static final String KW_120 = "120kw";
+
+    public static final String G_5 = "5 gears";
+    public static final String G_6 = "6 gears";
+    public static final String G_7 = "7 gears";
+
+    public static final String WHEELS_4 = "4 wheels drive";
+
+    public static final String MAX_120 = "120kw max";
+
+
+
+
 
 
     public  enum EngineType{
@@ -33,6 +53,14 @@ public class Utils {
         HIGH,
         SPORT
     }
+
+    public enum Transmission {
+        MANUEL,
+        AUTOMATIC,
+        SEQUENTIAL,
+        CONVERTER
+    }
+
 
 
 }
