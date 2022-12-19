e7b5c8d5155301ca66a0c1eeada30782266415df
yazid <aiman.aouad@etudiant.univ-rennes1.fr>
2022-12-19 20:20:42 +0100

    Ajout de la classe de teste partExteriorTest

--- src/fr/istic/tests/partExteriorTest.java
+++ src/fr/istic/tests/partExteriorTest.java
@@ -1,10 +1,16 @@
 package fr.istic.tests;
 
+import fr.istic.Utils;
 import fr.istic.issaouad.impl.*;
 import fr.istic.nplouzeau.cartaylor.api.Category;
-import fr.istic.nplouzeau.cartaylor.api.Configurator;
 import fr.istic.nplouzeau.cartaylor.api.PartType;
 import org.junit.Test;
+
+import javax.swing.*;
+
+import java.util.Optional;
+
+import static org.junit.Assert.assertEquals;
 
 public class partExteriorTest {
 
@@ -17,6 +23,12 @@ public class partExteriorTest {
         PartType xm = new PartTypeImpl("XM",Exterior.XM.class,exterior);
         PartType xs = new PartTypeImpl("XS",Exterior.XS.class,exterior);
 
-             
+        PartImpl XCInstance = xc.newInstance();
+        PartImpl XMInstance = xm.newInstance();
+        PartImpl XSInstance = xs.newInstance();
+
+        assertEquals(XCInstance.getProperty(Utils.PAINT), Optional.of(Utils.Color.CLASSIC.name()) );
+        assertEquals(XMInstance.getProperty(Utils.PAINT), Optional.of(Utils.Color.METALLIC.name()) );
+        assertEquals(XSInstance.getProperty(Utils.PAINT).get(), Utils.Color.RED.name() );
     }
 }
